c6d56679d1f0195909d6055ee02f1fbb8ad5cdad
Yuqi Zhang <yanwuzhidie@sina.com>
2016-07-22 22:30:53 +0100

    Creation

---
+++ src/lck00_J_CSPFLO/SomeObject.java
@@ -0,0 +1,12 @@
+package lck00_J_CSPFLO;
+
+public class SomeObject {
+
+	private final Object lock = new Object(); // private final lock object
+	public void changeValue() {
+		synchronized (lock) { // Locks on the private Object
+// ... 
+		}
+	} 
+	
+}
